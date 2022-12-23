fddb235d8eb1d466289bce8dbaf5bf2d0c385fc8
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2022-12-23 17:13:33 +0100

    TEST MESSAGE MODIFY OR ADD

---
+++ NewFileTEST.cs
@@ -0,0 +1,16 @@
+
+using UnityEngine;
+using System.Collections;
+
+
+public class MyCustomModifiedScript : MonoBehaviour {
+    // I MODIFIED THIS SCRIPT AS A TEST
+
+	// Use this for initializationvoid Start () {
+	
+	}
+	
+	// Update is called once per framevoid Update () {
+	
+	}
+}
