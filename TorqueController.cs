c3a8836a6238e6ff8ab3e215a32831f14725c207
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-03-05 21:52:14 +0100

    FROM REAL TIME CODING

---
+++ TorqueController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorqueController : ScriptableObj
+{
+    public float Torque;
+    public float Strength;
+    public bool IsOn;
+    private int index;
+
+    private void Spin()
+    {
+        Debug.Log("Im spinning");
+    }
+}
