9e4b74e347cb649c708090e12ee5594ac66e1b9a
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-03-10 00:27:00 +0100

    FROM REAL TIME CODING

--- LightMover.cs
+++ LightMover.cs
@@ -30,11 +30,41 @@ public class LightMover : ScriptableObj
     {
         if (Button(input,"m"))
         {
-            coroutineQueue.Enqueue(GoUp(5));
-            coroutineQueue.Enqueue(GoDown(1));
-            coroutineQueue.Enqueue(GoRight(15));
+            // u = Up, d = Down, l = Left, r = Right, b = Back, f = Forward
+            Go("u",10);
+            Go("l",10);
+            Go("f",20);
+
             
             StartCoroutine(CoroutineCoordinator());
+        }
+    }
+
+    private void Go(string direction,float seconds)
+    {
+        switch (direction)
+        {
+            case "u":
+                coroutineQueue.Enqueue(GoUp(seconds));
+                break;
+            case "d":
+                coroutineQueue.Enqueue(GoDown(seconds));
+                break;
+            case "l":
+                coroutineQueue.Enqueue(GoLeft(seconds));
+                break;
+            case "r":
+                coroutineQueue.Enqueue(GoRight(seconds));
+                break;
+            case "b":
+                coroutineQueue.Enqueue(GoBack(seconds));
+                break;
+            case "f":
+                coroutineQueue.Enqueue(GoForward(seconds));
+                break;
+            default:
+                // code to handle an invalid direction input
+                break;
         }
     }
     
@@ -61,8 +91,30 @@ public class LightMover : ScriptableObj
         yield return new WaitForSeconds(sec);
         isGoing = false;
     }
-
     
+    IEnumerator GoLeft(float sec)
+    {
+        isGoing = true;
+        Direction = Vector3.left;
+        yield return new WaitForSeconds(sec);
+        isGoing = false;
+    }
+
+    IEnumerator GoBack(float sec)
+    {
+        isGoing = true;
+        Direction = Vector3.back;
+        yield return new WaitForSeconds(sec);
+        isGoing = false;
+    }
+    
+    IEnumerator GoForward(float sec)
+    {
+        isGoing = true;
+        Direction = Vector3.forward;
+        yield return new WaitForSeconds(sec);
+        isGoing = false;
+    }
     
     private void FixedUpdate()
     {
