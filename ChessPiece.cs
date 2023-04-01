0c012db60db6fd6064572f9b8234062c69bef52b
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-04-01 13:40:21 +0200

    FROM REAL TIME CODING

--- ChessPiece.cs
+++ ChessPiece.cs
@@ -7,12 +7,12 @@ public class ChessPiece : ScriptableObj
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("I am " + gameObject.name);
     }
 
     // Update is called once per frame
     void Update()
     {
-       transform.rotation = new Quaternion(0,0,0,0); 
+        
     }
 }
