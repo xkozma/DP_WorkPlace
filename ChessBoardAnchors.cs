53b0c9ca1af0dcf7e1e3f37a048f6eeebfbddacb
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-04-04 22:53:49 +0200

    FROM REAL TIME CODING

--- ChessBoardAnchors.cs
+++ ChessBoardAnchors.cs
@@ -7,22 +7,12 @@ public class ChessBoardAnchors : ScriptableObj
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "ChessPieces")
+        if (other.tag == "ChessPiece")
         {
             float x = Mathf.RoundToInt(other.transform.localPosition.x / 60) * 60;
             float z = Mathf.RoundToInt(other.transform.localPosition.z / 60) * 60;
-            other.transform.localPosition = new Vector3(x,
-                0,
-                z);
+            other.transform.localPosition = new Vector3(x, 0, z);
         }
 
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "ChessPiece")
-        {
-            //Debug.Log("Exited " + other.name);
-        }
     }
 }
