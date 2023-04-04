52e8c2db8e13f1243582f26da2ac5fbb62fed755
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-04-04 22:34:57 +0200

    FROM REAL TIME CODING

--- ChessPiece.cs
+++ ChessPiece.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Enumerators;
 using UnityEngine;
 
 public class ChessPiece : ScriptableObj
 {
+    // One from: [Pawn,Rook,Queen,Bishop,King,Knight]
+    public PieceType PieceType;
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("I am " + gameObject.name);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
