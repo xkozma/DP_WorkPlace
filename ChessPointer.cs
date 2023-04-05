b30e3c0dd8f570e4756e9ea736af107becaa8b96
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-04-05 17:56:36 +0200

    FROM REAL TIME CODING

---
+++ ChessPointer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using static OVRInput;
+
+public class ChessPointer : ScriptableObj
+{
+    public Transform HandPosition;
+    private Renderer Renderer;
+
+    void Start()
+    {
+        Renderer = GetComponentInChildren<Renderer>();
+    }
+    void Update()
+    {
+        if (Get(OVRInput.Button.PrimaryHandTrigger, Controller.RTouch) || Get(OVRInput.Button.SecondaryHandTrigger, Controller.LTouch))
+        {
+            Renderer.enabled = true;
+        }
+        else
+        {
+            Renderer.enabled = false;
+        }
+        transform.position = HandPosition.position;
+        transform.rotation = Quaternion.Euler(0,0,0);
+    }
+}
