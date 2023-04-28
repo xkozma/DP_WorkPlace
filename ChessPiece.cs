5cc27ba023b88549103daa4b29c6e8a7f4980ff9
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-04-28 16:08:17 +0200

    FROM REAL TIME CODING

--- ChessPiece.cs
+++ ChessPiece.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChessPiece : ScriptableObj
 {
     private int step;
-
+    priv
     // One from: [Pawn,Rook,Queen,Bishop,King,Knight]
     public PieceType PieceType;
 
