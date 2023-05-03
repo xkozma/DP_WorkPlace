bd4c06c277baf93b6d901abfd69f484543e7bc72
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-03 19:22:36 +0200

    FROM REAL TIME CODING

--- ChessBoardAnchors.cs
+++ ChessBoardAnchors.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 
 public class ChessBoardAnchors : ScriptableObj
 {
-    public int step = 1000;
+    public int step = 60;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ChessPiece")
         {
             float x = Mathf.RoundToInt(other.transform.localPosition.x / step) * step;
-
-           float z = Mathf.RoundToInt(other.transform.localPosition.z / step) * step;
+            float z = Mathf.RoundToInt(other.transform.localPosition.z / step) * step;
 
 other.transform.localPosition = new Vector3(x,0,z);
 	    // There should be 2 dimensions in which chess is played. Y is not the one.
@@ -21,11 +20,11 @@ other.transform.localPosition = new Vector3(x,0,z);
         }
     }
 
- /*   private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.tag == "ChessPiece")
         {
             Debug.Log("Exited " + other.name);
         }
-    }*/
+    }
 }
