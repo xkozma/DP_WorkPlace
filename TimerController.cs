87f18114d941898e605deca2aebf8f9765c463b5
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-05 15:34:10 +0200

    FROM REAL TIME CODING

--- TimerController.cs
+++ TimerController.cs
@@ -57,7 +57,7 @@ public class TimerController : ScriptableObj
         private void ClockPause()
         {
             isClockRunning = false;
-            timerBackground.Renderer=Color<Gray>;
+            timerBackground.Renderer=Color.Gray;
             // Why isnt the timer gray anymore?
 
             StopAllCoroutines();
