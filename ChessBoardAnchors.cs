a4465cebf51703d3dc362b0185e1c10e64c5c5d6
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-09 15:45:51 +0200

    FROM REAL TIME CODING

--- ChessBoardAnchors.cs
+++ ChessBoardAnchors.cs
@@ -12,6 +12,8 @@ public class ChessBoardAnchors : ScriptableObj
         if (other.tag == "ChessPiece")
         {
             float x = Mathf.RoundToInt(other.transform.localPosition.x / step) * step;
+            float z = Mathf.RoundToInt(other.transform.localPosition.z / step) * step;
+            other.transform.localPosition = new Vector3(x, 0, z);
 	    // There should be 2 dimensions in which chess is played. Y is not the one.
 	    // Also, apply the changes to the piece
         }
@@ -22,6 +24,7 @@ public class ChessBoardAnchors : ScriptableObj
         if (other.tag == "ChessPiece")
         {
             Debug.Log("Exited " + other.name);
+            
         }
     }
 }
