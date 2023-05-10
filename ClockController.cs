c5aaa7de717f2d701afecd106d20ac86945e4d58
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-10 14:51:12 +0200

    FROM REAL TIME CODING

--- ClockController.cs
+++ ClockController.cs
@@ -65,8 +65,8 @@ public class ClockController : ScriptableObj
             SetClockData();
             ClockEvents.PauseClockEvent.Invoke();
             ClockEvents.ChangeClockStateEvent.Invoke(currentClockState);
-            StartButton.SetActive(true)
-            TapButton.SetActive(false)
+            StartButton.SetActive(true);
+            TapButton.SetActive(false);
             // Missing button switches?
         }
         
@@ -75,8 +75,8 @@ public class ClockController : ScriptableObj
             SetClockData();
             ClockEvents.ChangePlayerEvent.Invoke(currentPlayerOnClock);
             ClockEvents.ChangeClockStateEvent.Invoke(currentClockState);
-            StartButton.SetActive(false)
-            TapButton.SetActive(true)
+            StartButton.SetActive(false);
+            TapButton.SetActive(true);
             // Missing button switches again?
         }
 
