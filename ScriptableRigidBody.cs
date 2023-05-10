eff77d963ee0189bc8fa06546c49effec9601893
Miroslav Kozma <xkozma@stuba.sk>
2023-05-10 16:35:38 +0200

    B

--- ScriptableRigidBody.cs
+++ ScriptableRigidBody.cs
@@ -26,8 +26,8 @@ public class ScriptableRigidBody : ScriptableObj
         }
         // We need to set MyRigidBody drag as well as we did it with Mass
         // Pascal case, Camel case convetions are the same
-       
-         if (MyRigidBody.drag != Drag)
+
+        if (MyRigidBody.drag != Drag)
         {
             MyRigidBody.drag = Drag;
         }
