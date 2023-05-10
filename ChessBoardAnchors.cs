6a30ee24509f41a88eae4847a9bb9213ef846a19
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-10 15:50:48 +0200

    FROM REAL TIME CODING

--- ChessBoardAnchors.cs
+++ ChessBoardAnchors.cs
@@ -12,10 +12,11 @@ public class ChessBoardAnchors : ScriptableObj
         if (other.tag == "ChessPiece")
         {
             float x = Mathf.RoundToInt(other.transform.localPosition.x / step) * step;
+            float y = Mathf.RoundToInt(other.transform.localPosition.y);
+            float z = Mathf.RoundToInt(other.transform.localPosition.z / step) * step;
 	    // There should be 2 dimensions in which chess is played. Y is not the one.
 	    // Also, apply the changes to the piece
-            float z = Mathf.RoundToInt(other.transform.localPosition.z / step) * step;
-other.transform.localPosition = new Vector3(x,0,z);
+            New Vector3(x,y,z);
         }
     }
 
