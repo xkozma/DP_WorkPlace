7b673476e04180e92fd333b1d748e0167e5b1cd4
Miroslav Kozma <43440463+xkozma@users.noreply.github.com>
2023-05-12 15:54:59 +0200

    FROM REAL TIME CODING

--- ChessPointer.cs
+++ ChessPointer.cs
@@ -15,7 +15,11 @@ public class ChessPointer : ScriptableObj
         if (Get(OVRInput.Button.PrimaryHandTrigger, Controller.RTouch) || Get(OVRInput.Button.PrimaryHandTrigger, Controller.LTouch))
         {
             // This is when we touch any of Grab Buttons
+             Renderer.enabled = true;
+            
         }
+        else
+            Renderer.enabled = false;
 
 
         if(HandPosition!=null)
